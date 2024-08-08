824aeddaa6195a7473282557b2cd3f2733955daf
JOAO ROBERTO DE OLIVEIRA FELIX <ROBERTO6262@DIGITALCOLLEGE.LOCAL>
2024-08-08 20:52:37 -0300

    Subindo todos os metodos

--- todos.js
+++ todos.js
@@ -39,18 +39,54 @@
 //  listaAllTodosById(2)
 
  //4 - Criar um recurso utilizando o REST API do jsonplaceholder
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            user: 2,
-            title: 'Teste',
-            body: 'Teste2',
-        })
-    })
-    .then(response => response.json())
-    .then(json => console.log(JSON.stringify(json, null, 4)))
+    // fetch('https://jsonplaceholder.typicode.com/posts', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //         user: 2,
+    //         title: 'Teste',
+    //         body: 'Teste2',
+    //     })
+    // })
+    // .then(response => response.json())
+    // .then(json => console.log(JSON.stringify(json, null, 4)))
+
+//5 - Atualizar o post com o id 54
+// fetch('https://jsonplaceholder.typicode.com/posts/54', {
+//     method: 'PUT',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//         user: 2,
+//         title: 'atualizado',
+//         body: 'body atualizado',
+//     })
+// })
+// .then(response => response.json())
+// .then(json => console.log(JSON.stringify(json, null, 4)))
+
+// 6 - Atualizar o post parciamente
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: 'PATCH',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//         title: 'Titulo Post Atualizado',
+//     })
+// })
+// .then(response => response.json())
+// .then(json => console.log(JSON.stringify(json, null, 4)))
+
+
+// 7 - Deletar o Post com o id 22
+fetch('https://jsonplaceholder.typicode.com/posts/22', {
+    method: 'DELETE',
+})
+.then(response => response.json())
+.then(json => console.log(JSON.stringify(json, null, 4)))
  
 
